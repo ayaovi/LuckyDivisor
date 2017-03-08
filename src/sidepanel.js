9a21ca602ddd420118fe02f030d7d9caebc8c42d
ayaovi <djayespoiro@gmail.com>
2017-03-08 07:24:40 +0200

    working on it...

--- src/sidepanel.js
+++ src/sidepanel.js
@@ -29,6 +29,11 @@ class SidePanel extends Panel
 		 * The list of pn cubes that the player needs to collect.
 		 */
 		this.pnCubesYetToBeCollected;
+
+		/**
+		 * A map of pn cube number and the number of instances to be collected.
+		 */
+		this.numberOfPnCubesYetToBeCollected;
 	}
 
 	/**
@@ -43,7 +48,26 @@ class SidePanel extends Panel
 		this.position = createVector(WIDTH_OF_CANVAS, 0);
 		this.width = WIDTH_OF_SIDE_PANEL;
 		this.height = HEIGHT_OF_SIDE_PANEL;
+		this.reset();
+	}
+
+
+	/**
+	 * @description 
+	 *
+	 * @param 
+	 *
+	 * @return 
+	 */
+	reset()
+	{
 		this.pnCubesYetToBeCollected = [];
+		this.numberOfPnCubesYetToBeCollected = {};
+		this.numberOfPnCubesYetToBeCollected[1] = 0;
+		this.numberOfPnCubesYetToBeCollected[2] = 0;
+		this.numberOfPnCubesYetToBeCollected[3] = 0;
+		this.numberOfPnCubesYetToBeCollected[5] = 0;
+		this.numberOfPnCubesYetToBeCollected[7] = 0;
 		var divisors = playerCube.divisors;
 
 		/**
@@ -51,9 +75,13 @@ class SidePanel extends Panel
 		 */
 		for (var i = 0; i < divisors.length; i++)
 		{
-			this.pnCubesYetToBeCollected.push(new PnCube(divisors[i], ++ID, 
-				createVector(this.position.x + DEFAULT_SIDE_PANEL_PADDING, 
-					DEFAULT_SIDE_PANEL_PADDING + i * (DEFAULT_SIDE_PANEL_PADDING + SIDE_OF_CUBE))));
+			if (this.numberOfPnCubesYetToBeCollected[divisors[i]] == 0)
+			{
+				this.pnCubesYetToBeCollected.push(new PnCube(divisors[i], ++ID, 
+					createVector(this.position.x + DEFAULT_SIDE_PANEL_PADDING, 
+						DEFAULT_SIDE_PANEL_PADDING + i * (DEFAULT_SIDE_PANEL_PADDING + SIDE_OF_CUBE))));
+			}
+			this.numberOfPnCubesYetToBeCollected[divisors[i]]++;
 		}
 	}
 
@@ -86,7 +114,13 @@ class SidePanel extends Panel
 		 */
 		for (var i = 0; i < this.pnCubesYetToBeCollected.length; i++) 
 		{
-			this.pnCubesYetToBeCollected[i].show();
+			var cube = this.pnCubesYetToBeCollected[i];
+			var number = this.numberOfPnCubesYetToBeCollected[cube.number];
+			if (number != 0)
+			{
+				cube.show();
+				text("x" + number, cube.position.x + SIDE_OF_CUBE, cube.position.y + SIDE_OF_CUBE);
+			}
 		}
 	}
 
@@ -103,6 +137,7 @@ class SidePanel extends Panel
 		/**
 		 * Remove last pn cube with prime number the supplied number.
 		 */
-		removeCubeFromYetToBeCollecteList(this.pnCubesYetToBeCollected, number);
+		// removeCubeFromYetToBeCollecteList(this.pnCubesYetToBeCollected, number);
+		--this.numberOfPnCubesYetToBeCollected[number];
 	}
 }
