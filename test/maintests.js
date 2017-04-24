710e981d6f0bde28b610a89d604a88847614b27c
ayaovi <djayespoiro@gmail.com>
2017-04-24 06:49:15 +0200

    chore: extend global draw function test
    Add test to confirm that current world save is indeed taking place during the draw() loop.

--- test/maintests.js
+++ test/maintests.js
@@ -9,11 +9,14 @@
  */
 
 
-QUnit.test("global draw function test", function( assert ) {
+QUnit.test("global draw function test", function(assert) {
     luckyDivisor.util.createGameComponents();
     luckyDivisor.global.currentWorld.reset();
+    luckyDivisor.global.currentWorld.topPanel.clock.stringTimeTillEndOfPlay = "00:15";
+    assert.equal(luckyDivisor.global.worlds.length, 1, "before draw() execution, the number of worlds should be 1");
 
     draw();
 
-    assert.ok(true, "This mainly to test if anything fails along the way.")
+    assert.ok(true, "This mainly to test if anything fails along the way.");
+    assert.equal(luckyDivisor.global.worlds.length, 2, "after draw() execution, the number of worlds should increase by 1");
 });
